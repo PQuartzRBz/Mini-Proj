12d54bcba98fbf48afb363ca7805cc3597f2e180
PQuartzRBz <63011240@kmitl.ac.th>
2022-11-15 10:36:32 +0700

    servercom

--- serverComm.py
+++ serverComm.py
@@ -1,9 +1,20 @@
 ###  function start here ###
+import requests
+import json
+url = "endpoint"
 def validate_car(lp):
     # Sent str to server
     # NotImplemented
-    lp = '' #
+    payload = json.dumps(
+        {
+            "license_str": lp
+            })
     # your code
+    headers = {
+            'Content-Type' : 'application/json'
+            }
+    response = requests.request("POST",url,headers = headers, data= payload)
+    print(response.text)
     return True
 
 ###  function end here ###
