ff434c9be85149ceda399217e3154060e0b3e1c0
Passakorn Pattarapakorn <63011240@kmitl.ac.th>
2022-11-15 10:32:13 +0700

    modified:   main.py 	modified:   carDetection.py 	modified:   gateUpdate.py

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
-###  setup start here ###
+###  import start here ###
 import time
-from carDetection import get_car
+from carDetection import Detect
 from gateUpdate import update_gate_state
 from serverComm import validate_car
 from licenseReading import get_license
 # connect to server API
-###  setup end here ###
+###  import end here ###
 
 ### Pins setup start here ###
-input_metal = 1
-input_optic = 2
-output_open = None
-output_close = None
+input_metal = 11
+input_optic = 12
+output_led = 13
+output_open = 14
+output_close = 15
 ### Pins setup end here ###
 
-###  system init start here ###
+###  system setup start here ###
 work = True
 state_gate = False # init as close/False
 state_metal = False
@@ -22,17 +23,18 @@ state_optic = False
 timer_metal = 200
 timer_optic = 200
 dt = 0
-###  system init end here ###
+
+mySensor = Detect(output_led,input_metal,input_optic)
+###  system setup end here ###
 
 
 ###  main start here ###
 while(work):
     timeAtStartLoop = time.time_ns()
 
-    isCar = get_car(input_metal, input_optic, dt) # GPIO
-    if(isCar):
-        lp = get_license() # AI
-        if(validate_car(lp)): # API
+    if(mySensor.get_car()):
+        licenseNumber = get_license() # AI
+        if(validate_car(licenseNumber)): # API
             state_gate = True
             print('Welcome')
         else:
